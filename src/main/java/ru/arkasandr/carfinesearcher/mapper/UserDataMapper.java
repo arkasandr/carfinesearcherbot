daa13f29a67cde4da775bafcbc6e0e62fed81ba4
arkasandr <arkasandr@gmail.com>
2022-07-31 22:47:26 +0300

    220731 - проброс captcha из парсера через очередь в сервиса бота

--- src/main/java/ru/arkasandr/carfinesearcher/mapper/UserDataMapper.java
+++
@@ -1,1 +0,0 @@
-package ru.arkasandr.carfinesearcher.mapper;import org.mapstruct.Mapper;import org.mapstruct.factory.Mappers;import ru.arkasandr.carfinesearcher.model.UserData;@Mapperpublic interface UserDataMapper {    UserDataMapper INSTANCE = Mappers.getMapper(UserDataMapper.class);    UserDataDto toDto(UserData data);    UserData toEntity(UserDataDto dto);}
